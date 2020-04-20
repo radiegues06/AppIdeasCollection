486e06aa1356ba21310a6d82790b88b69ad1daf9
rafaelamanciodiegues@gmail.com <ri65%ma58R>
2020-04-20 18:59:29 -0300

    Algorithm calculation correction

--- Beginner Projects/Dollars2Cents/src/main/Algorithm.java
+++ Beginner Projects/Dollars2Cents/src/main/Algorithm.java
@@ -6,48 +6,48 @@ import java.util.Map;
 
 public class Algorithm {
 
-    private static LinkedHashMap<String, Double> moneyValues = new LinkedHashMap<>();
+    private static LinkedHashMap<String, Integer> moneyValues = new LinkedHashMap<>();
     private static HashMap<String, Integer> distribution = new HashMap<>();
 
     public static HashMap<String,Integer> calculateMoneyDistribution(String moneyUserInput) {
         createMoneyValuesMap();
 
-        double userAmountInput = convertMoneyInputToDouble(moneyUserInput);
+        double userAmountInput = convertMoneyInputToInteger(moneyUserInput);
 
-        for (Map.Entry<String , Double> entry: moneyValues.entrySet()) {
+        for (Map.Entry<String , Integer> entry: moneyValues.entrySet()) {
             String key = entry.getKey();
-            Double moneyValue = entry.getValue();
+            Integer moneyValue = entry.getValue();
 
-            //TODO consider float division. It is doing it worng. E.g. 17,70
+            int amountOfMoneyValue = (int) (userAmountInput / moneyValue);
 
-            double amountOfMoneyValue = userAmountInput / moneyValue;
+            distribution.put(key, amountOfMoneyValue);
 
-            distribution.put(key, (int) amountOfMoneyValue);
+            userAmountInput -= amountOfMoneyValue * moneyValue;
 
-            userAmountInput -= (int) amountOfMoneyValue * moneyValue;
+            System.out.println(userAmountInput);
         }
 
         System.out.println(distribution.toString());
         return distribution;
     }
 
-    public static double convertMoneyInputToDouble(String moneyUserInput) {
-        return Double.parseDouble(InputValidator.getInputDigits(moneyUserInput)) / 100.0;
+    public static double convertMoneyInputToInteger(String moneyUserInput) {
+        return Integer.parseInt(InputValidator.getInputDigits(moneyUserInput));
     }
 
     private static void createMoneyValuesMap() {
-        moneyValues.put("Reais100", 100.0);
-        moneyValues.put("Reais50", 50.0);
-        moneyValues.put("Reais20", 20.0);
-        moneyValues.put("Reais10", 10.0);
-        moneyValues.put("Reais5", 5.0);
-        moneyValues.put("Reais2", 2.0);
-        moneyValues.put("Reais1", 1.0);
-        moneyValues.put("Cents50", 0.50);
-        moneyValues.put("Cents25", 0.25);
-        moneyValues.put("Cents10", 0.10);
-        moneyValues.put("Cents5", 0.05);
-        moneyValues.put("Cents1", 0.01);
+        moneyValues.put("Reais100", 10000);
+        moneyValues.put("Reais50", 5000);
+        moneyValues.put("Reais20", 2000);
+        moneyValues.put("Reais10", 1000);
+        moneyValues.put("Reais5", 500);
+        moneyValues.put("Reais2", 200);
+        moneyValues.put("Reais1", 100);
+        moneyValues.put("Cents50", 50);
+        moneyValues.put("Cents25", 25);
+        moneyValues.put("Cents10", 10);
+        moneyValues.put("Cents5", 5);
+        moneyValues.put("Cents1", 1);
     }
 
 }
